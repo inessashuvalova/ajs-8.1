4004705f98a5eafc0e3c81f8a6d70450c92a5205
Inessa Shuvalova <inessa.shuvalova@mail.ru>
2021-07-12 20:10:13 +0300

    initial commit

---
+++ src/js/Team.js
@@ -0,0 +1,23 @@
+export default class Team {
+    constructor() {
+      this.members = new Set();
+    }
+  
+    add(character) {
+      if (!this.members.has(character)) {
+        this.members.add(character);
+      } else {
+        throw new Error(`Character ${character.name} was added into the Team already`);
+      }
+    }
+  
+    addAll(...members) {
+      members.forEach((member) => {
+        this.members.add(member);
+      });
+    }
+  
+    toArray() {
+      return Array.from(this.members);
+    }
+  }
